b3592e36d8fc0e7e2ffb6766b8337a6c0ea69d63
Cameron Griffiths <camgriff01@gmail.com>
2022-02-12 11:22:45 -0800

    Update to Main.py, testing webcam tutorials for opencv. This doesn't work but I believe it is due to virtual machine camera issues etc. Commiting to test when Raspberry PI has been initialised :)

--- main.py
+++ main.py
@@ -1,14 +1,11 @@
-import numpy as np
 import cv2
 
 cap = cv2.VideoCapture(0)
+cap.set(3,640)
+cap.set(4,480)
 
-while(True):
-    ret, frame = cap.read()
-
-    cv2.imshow('frame', frame)
-    if cv2.waitKey(20) & 0xFF == ord('q'):
+while True:
+    success, img = cap.read()
+    cv2.imshow("Video", img)
+    if cv2.waitKey(1) & 0xFF ==ord('q'):
         break
-
-cap.release()
-cv2.destroyAllWindows()
