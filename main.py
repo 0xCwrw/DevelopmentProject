22f837c3bd4122e3c7bb84e1684482c3af9bf034
Cameron Griffiths <camgriff01@gmail.com>
2022-04-08 20:01:06 +0100

    - Facial recognition now working, identifies me as "cam" from the dataset of 10 images.

--- main.py
+++ main.py
@@ -1,39 +1,58 @@
-import cv2 as cv
 import numpy as np
 import cv2
+import pickle
+import time
 
-img = cv.imread('test.jpeg')
-cv.imshow('Adam Price', img)
+face_cascade = cv2.CascadeClassifier('cascades/haarcascade_frontalface_default.xml')
+recogniser = cv2.face.LBPHFaceRecognizer_create()
+recogniser.read("recognisers/face-trainner.yml")
 
-# Converting to greyscale
-gray = cv.cvtColor(img, cv.COLOR_BGR2GRAY)
-cv.imshow('Gray', gray)
-#Get user supplied values
-imagePath = "test.jpeg"
-cascPath = "cascades/haarcascade_frontalface_default.xml"
+labels = {"person_name": 1}
+with open("identities/face-labels.pickle", 'rb') as f:
+	og_labels = pickle.load(f)
+	labels = {v:k for k,v in og_labels.items()}
 
+cap = cv2.VideoCapture(0, cv2.CAP_V4L2)
+time.sleep(0.1)
 
-#Create the haar cascade
-faceCascade = cv2.CascadeClassifier(cascPath)
+while(True):
+    # Capture frame-by-frame
+    ret, frame = cap.read()
+    frame = cv2.flip(frame, 0)
+    gray  = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
+    faces = face_cascade.detectMultiScale(gray, scaleFactor=1.5, minNeighbors=5)
+    for (x, y, w, h) in faces:
+        print(x,y,w,h)
+        roi_gray = gray[y:y+h, x:x+w] #(ycord_start, ycord_end)
+        roi_color = frame[y:y+h, x:x+w]
 
-#Read the image
-image = cv2.imread(imagePath)
-gray = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
+    	# recognize? deep learned model predict keras tensorflow pytorch scikit learn
+        id_, conf = recogniser.predict(roi_gray)
+        if conf>=4 and conf <= 85:
+            #print(5: #id_)
+            #print(labels[id_])
+            font = cv2.FONT_HERSHEY_SIMPLEX
+            name = labels[id_]
+            color = (255, 255, 255)
+            stroke = 2
+            cv2.putText(frame, name, (x,y), font, 1, color, stroke, cv2.LINE_AA)
+            
+        # img_item = "7.png"
+        # cv2.imwrite(img_item, roi_color)
+        
+        color = (255, 0, 0) #BGR 0-255 
+        stroke = 2
+        end_cord_x = x + w
+        end_cord_y = y + h
+        cv2.rectangle(frame, (x, y), (end_cord_x, end_cord_y), color, stroke)
+    	#subitems = smile_cascade.detectMultiScale(roi_gray)
+    	#for (ex,ey,ew,eh) in subitems:
+    	#	cv2.rectangle(roi_color,(ex,ey),(ex+ew,ey+eh),(0,255,0),2)
+    # Display the resulting frame
+    cv2.imshow('frame',frame)
+    if cv2.waitKey(20) & 0xFF == ord('q'):
+        break
 
-#Detect faces in the image
-faces = faceCascade.detectMultiScale(
-    gray,
-    scaleFactor=1.1,
-    minNeighbors=5,
-    minSize=(30,30),
-    flags= cv2.CASCADE_SCALE_IMAGE
-)
-
-print("Hey dude, found {0} faces in that image :)".format(len(faces)))
-
-#draws a rectangle around faces
-for (x, y, w, h) in faces:
-    cv2.rectangle(image, (x, y), (x+w, y+h), (0, 255, 0), 2)
-
-cv2.imshow("Faces found", image)
-cv2.waitKey(0) 
+# When everything done, release the capture
+cap.release()
+cv2.destroyAllWindows()
