bb90b4400bde08febd0b58686c10dea65c0840d7
Cameron Griffiths <camgriff01@gmail.com>
2022-04-13 18:58:38 +0100

    - Testing to save images of detected faces.

--- dir-test.py
+++ dir-test.py
@@ -1,11 +1,40 @@
 import os
+import cv2
+from time import sleep
 
-parent_directory = "dataset/"
-userName = "cam"
-path = os.path.join(parent_directory, userName)
+face_cascade = cv2.CascadeClassifier('cascades/haarcascade_frontalface_default.xml')
+cap = cv2.VideoCapture(0, cv2.CAP_V4L2)
+newUser = "cameron"
 
-isdir = os.path.isdir(path)
+def save_frame(frame):
+    if len(faces) >= 1:
+        cv2.imwrite('dataset/test{}.png'.format(i), frame)
 
-print(isdir)
-# os.mkdir(path)
-# print("Directory {} has been created.\n".format(path))
+while(True):
+    # Capture frame-by-frame
+    ret, frame = cap.read()
+    frame = cv2.flip(frame, 0)
+    gray  = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
+    faces = face_cascade.detectMultiScale(gray, scaleFactor=1.5, minNeighbors=5)
+
+    # for (x, y, w, h) in faces:
+    #     # print(x,y,w,h)
+    #     roi_gray = gray[y:y+h, x:x+w] #(ycord_start, ycord_end)
+    #     roi_color = frame[y:y+h, x:x+w]
+    #     color = (255, 0, 0) #BGR 0-255 
+    #     stroke = 2
+    #     end_cord_x = x + w
+    #     end_cord_y = y + h
+
+    #     cv2.rectangle(frame, (x, y), (end_cord_x, end_cord_y), color, stroke)
+    for i in range(1, 11):
+        save_frame(frame)
+    # print("found {0} faces!".format(len(faces)))
+
+    # Display the resulting frame
+    cv2.imshow(newUser,frame)
+    if cv2.waitKey(20) & 0xFF == ord('q'):
+        break
+# When everything done, release the capture
+cap.release()
+cv2.destroyAllWindows()
