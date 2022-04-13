105160d5c6b794bb91189b128d6c7c7a9cf79664
Cameron Griffiths <camgriff01@gmail.com>
2022-04-13 18:58:55 +0100

    - Edits made for recipt of successful testing

--- face_registration.py
+++ face_registration.py
@@ -61,11 +61,26 @@ else:
     directory_creation(newUser)
     print("Hello {},\nWe will now begin by scanning your face.".format(newUser))
 
-# while(True):
-#     ret, frame = cap.read()
-#     frame = cv2.flip(frame, 0)
-#     cv2.imshow(newUser,frame)
-#     if cv2.waitKey(20) & 0xFF == ord('q'):
-#         break
-# cap.release()
-# cv2.destroyAllWindows()
+while(True):
+    # Capture frame-by-frame
+    ret, frame = cap.read()
+    frame = cv2.flip(frame, 0)
+    gray  = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
+    faces = face_cascade.detectMultiScale(gray, scaleFactor=1.5, minNeighbors=5)
+    for (x, y, w, h) in faces:
+        print(x,y,w,h)
+        roi_gray = gray[y:y+h, x:x+w] #(ycord_start, ycord_end)
+        roi_color = frame[y:y+h, x:x+w]
+        color = (255, 0, 0) #BGR 0-255 
+        stroke = 2
+        end_cord_x = x + w
+        end_cord_y = y + h
+        cv2.rectangle(frame, (x, y), (end_cord_x, end_cord_y), color, stroke)
+    # Display the resulting frame
+    cv2.imshow(newUser,frame)
+    if cv2.waitKey(20) & 0xFF == ord('q'):
+        break
+
+# When everything done, release the capture
+cap.release()
+cv2.destroyAllWindows()
