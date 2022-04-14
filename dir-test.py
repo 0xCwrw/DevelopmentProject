2a6b9a305679288f2a44b15ae2ba33503b800d88
Cameron Griffiths <camgriff01@gmail.com>
2022-04-14 09:35:46 +0100

    - Method to add faces successful.

--- dir-test.py
+++ dir-test.py
@@ -6,35 +6,36 @@ face_cascade = cv2.CascadeClassifier('cascades/haarcascade_frontalface_default.x
 cap = cv2.VideoCapture(0, cv2.CAP_V4L2)
 newUser = "cameron"
 
-def save_frame(frame):
-    if len(faces) >= 1:
-        cv2.imwrite('dataset/test{}.png'.format(i), frame)
+def save_frame(frame, i):
+    cv2.imwrite('dataset/test{}.png'.format(i), frame)
 
-while(True):
-    # Capture frame-by-frame
-    ret, frame = cap.read()
-    frame = cv2.flip(frame, 0)
-    gray  = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
-    faces = face_cascade.detectMultiScale(gray, scaleFactor=1.5, minNeighbors=5)
-
-    # for (x, y, w, h) in faces:
-    #     # print(x,y,w,h)
-    #     roi_gray = gray[y:y+h, x:x+w] #(ycord_start, ycord_end)
-    #     roi_color = frame[y:y+h, x:x+w]
-    #     color = (255, 0, 0) #BGR 0-255 
-    #     stroke = 2
-    #     end_cord_x = x + w
-    #     end_cord_y = y + h
-
-    #     cv2.rectangle(frame, (x, y), (end_cord_x, end_cord_y), color, stroke)
+def image_collection():
     for i in range(1, 11):
-        save_frame(frame)
-    # print("found {0} faces!".format(len(faces)))
-
-    # Display the resulting frame
-    cv2.imshow(newUser,frame)
-    if cv2.waitKey(20) & 0xFF == ord('q'):
-        break
+        sleep(2)
+        while(True):
+            # Capture frame-by-frame
+            ret, frame = cap.read()
+            frame = cv2.flip(frame, 0)
+            gray  = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
+            faces = face_cascade.detectMultiScale(gray, scaleFactor=1.5, minNeighbors=5)
+            x = 0
+            if len(faces) == 1:
+                save_frame(frame, i)
+                print("Face detected, sample {}/10 successful.".format(i))
+                break
+            elif len(faces) < 1:
+                print("No faces detected, please adjust.")
+            else:
+                print("Too many faces detected, there should only be one person in frame.")
+            # Display the resulting frame
+        if cv2.waitKey(20) & 0xFF == ord('q'):
+            x = 1
+            print("Sample collection unsuccessful.")
+            break
+    if x == 0:
+        print("sample collection successful...\nUser {} added to dataset.".format(newUser))
+    else:
+        print("\n")
 # When everything done, release the capture
-cap.release()
-cv2.destroyAllWindows()
+# cap.release()
+# cv2.destroyAllWindows()
