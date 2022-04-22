3b029a38e9d083a2ecc433237c51c3f8b371b772
Cameron Griffiths <P2515747@my365.dmu.ac.uk>
2022-04-22 22:57:26 +0100

    Set face recognition test to new main.

--- testing-scripts/face_recognition_test.py
+++ testing-scripts/face_recognition_test.py
@@ -22,24 +22,20 @@ while(True):
     gray  = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
     faces = face_cascade.detectMultiScale(gray, scaleFactor=1.5, minNeighbors=5)
     for (x, y, w, h) in faces:
-        print(x,y,w,h)
+        # print(x,y,w,h)
         roi_gray = gray[y:y+h, x:x+w] #(ycord_start, ycord_end)
         roi_color = frame[y:y+h, x:x+w]
 
         #After prediction of ROI (Region of interest) computation is focused in that area.
         id_, conf = recogniser.predict(roi_gray)
-        if conf>=4 and conf <= 85:
-            #print(5: #id_)
-            #print(labels[id_])
+        if conf>=40 and conf <= 80:
             font = cv2.FONT_HERSHEY_SIMPLEX
             name = labels[id_]
             color = (255, 255, 255)
             stroke = 2
+            confidence = str(conf)
             cv2.putText(frame, name, (x,y), font, 1, color, stroke, cv2.LINE_AA)
-            
-        # img_item = "7.png"
-        # cv2.imwrite(img_item, roi_color)
-        
+            cv2.putText(frame, confidence, (y,x), font, 1, color, stroke, cv2.LINE_AA)
         color = (255, 0, 0) #BGR 0-255 
         stroke = 2
         end_cord_x = x + w
@@ -49,7 +45,7 @@ while(True):
     	#for (ex,ey,ew,eh) in subitems:
     	#	cv2.rectangle(roi_color,(ex,ey),(ex+ew,ey+eh),(0,255,0),2)
     # Display the resulting frame
-    cv2.imshow('frame',frame)
+    cv2.imshow('Face detection',frame)
     if cv2.waitKey(20) & 0xFF == ord('q'):
         break
 
