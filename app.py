c01e65dc0cd55c1574941d35d5bba02aef0af155
Cameron Griffiths <camgriff01@gmail.com>
2022-04-27 19:03:27 +0100

    Progress on GUI

--- app.py
+++ app.py
@@ -1,75 +1,136 @@
-from cProfile import label
-from tkinter import *
+import tkinter as tk
 from tkinter import messagebox
-import os
-# from guiTest import name_collection
+from tkinter import ttk
 
-root = Tk()
-root.title('Facial recognition GUI')
-root.geometry("500x350")
+from numpy import roots
 
-def openFaceReg():
-    faceReg = Toplevel()
-    faceReg.title('Face registration')
-    faceReg.geometry("500x350")
-    welcome = Label(faceReg, text ="Welcome, please enter name.").pack()
 
-    def name_collection():
-        if(any(x.isalpha() for x in player_name.get())
-        and all(x.isalpha() or x.isspace() for x in player_name.get())):
-            username = player_name.get()
-            username = username.lower()
-            username = username.replace(" ","-")
-            parent_directory = "dataset/"
-            path = os.path.join(parent_directory, username)
-            if os.path.isdir(path):
-                messagebox.showerror("Existing user", "This user is already a part of the system.")
+class LoginPage(tk.Tk): # modify for face recognition instead of pswd, then add if fails to use pswd
+    def __init__(self, *args, **kwards):
+        tk.Tk.__init__(self, *args, **kwards)
+        
+        main_frame = tk.Frame(self, bg="#000", height = 431, width=626)
+        main_frame.pack(fill="both", expand = "true")
+
+        self.geometry("626x431")
+        self.resizable(0,0)
+
+        login_frame = tk.Frame(main_frame, bg="#FFF", relief="groove", bd=2)
+        login_frame.place(rely=0.30, relx=0.17, height=130, width=400)
+        
+        label_title = tk.Label(login_frame, text="Login Page")
+        label_title.grid(row=0, column=1, columnspan=1)
+
+        label_user = tk.Label(login_frame, text="Username:")
+        label_user.grid(row=1, column=0)
+
+        label_pw = tk.Label(login_frame, text="Password:")
+        label_pw.grid(row=2, column=0)
+
+        entry_user = ttk.Entry(login_frame, width=45, cursor="xterm")
+        entry_user.grid(row=1, column=1)
+
+        entry_pw = ttk.Entry(login_frame, width=45, cursor="xterm", show="*")
+        entry_pw.grid(row=2, column=1)
+
+        button = ttk.Button(login_frame, text="Login", command=lambda: getlogin())
+        button.place(rely=0.70, relx=0.50)
+
+        signup_btn = ttk.Button(login_frame, text="Register", command=lambda: get_signup())
+        signup_btn.place(rely=0.70, relx=0.75)
+
+        def get_signup():
+            SignupPage()
+        
+        def getlogin():
+            username = entry_user.get()
+            password = entry_pw.get()
+            validation = validate(username, password)
+            if validation:
+                tk.messagebox.showinfo("Login Successful", "Welcome {}".format(username))
+                root.deiconify()
+                top.destroy()
             else:
-                os.mkdir(path)
-                Label(faceReg, text=f'The directory {username} has been created.').pack()
-                #possiblly close this window and open one for image collection?
-        else:
-            messagebox.showerror("Invalid characters detected.", "Invalid characters in username\n letters and white space only.")
+                tk.messagebox.showerror("Information", "The username or password you have entered are incorrect ")
+        def validate(username, password):
+            try:
+                with open("credentials.txt", "r") as credentials:
+                    for line in credentials:
+                        line = line.split(",")
+                        if line [1] == username and line[3] == password:
+                            return True
+                    return False
+            except FileNotFoundError:
+                print("you need to register first")
+                return False
 
-# style this window and change variable names.
-    player_name = Entry(faceReg, width=30)
-    player_name.pack(pady=30)
+class SignupPage(tk.Tk):        # incorporate the face registration process here
+    def __init__(self, *args, **kwargs):
+        tk.Tk.__init__(self, *args, **kwargs)
 
+        main_frame = tk.Frame(self, bg="#3F6BAA", height=150, width=250)
+        # pack_propagate prevents the window resizing to match the widgets
+        main_frame.pack_propagate(0)
+        main_frame.pack(fill="both", expand="true")
+
+        self.geometry("250x150")
+        self.resizable(0, 0)
+
+        self.title("Registration")
+
+        label_user = tk.Label(main_frame, text="New Username:")
+        label_user.grid(row=1, column=0)
+
+        label_pw = tk.Label(main_frame, text="New Password:")
+        label_pw.grid(row=2, column=0)
+
+        entry_user = ttk.Entry(main_frame, width=20, cursor="xterm")
+        entry_user.grid(row=1, column=1)
+
+        entry_pw = ttk.Entry(main_frame, width=20, cursor="xterm", show="*")
+        entry_pw.grid(row=2, column=1)
+
+        button = ttk.Button(main_frame, text="Create Account", command=lambda: signup())
+        button.grid(row=4, column=1)
     
+        def signup():
+            user = entry_user.get()
+            pw = entry_pw.get()
+            validation = validate_user(user)
+            if not validation:
+                tk.messagebox.showerror("information", "Username already exists")
+            else:
+                if len(pw) > 3:
+                    credentials = open("credentials.txt", "a")
+                    credentials.write(f"Username,{user},Password,{pw},\n")
+                    credentials.close()
+                    tk.messagebox.showinfo("Information", "Your account details have been stored.")
+                    SignupPage.destroy(self)
+                else:
+                    tk.messagebox.showerror("information", "Your password is too short")
+        def validate_user(username):
+                # Checks the text file for a username/password combination.
+                try:
+                    with open("credentials.txt", "r") as credentials:
+                        for line in credentials:
+                            line = line.split(",")
+                            if line[1] == username:
+                                return False
+                    return True
+                except FileNotFoundError:
+                    return True
+class MyApp(tk.Tk): # no idea what to put here
+
+    def __init__(self, *args, **kwargs):
+
+        tk.Tk.__init__(self, *args, **kwargs)
+        ttk.Label(self, text="Hello you are in my app").pack()
+
+top = LoginPage()
+top.title("Face recognition app - Login")
 
 
-    Button(faceReg, text="register player", padx=10, pady=5, command= name_collection).pack()
-    
-    
-    
-
-
-
-
-# def faceReg_window():
-#     # Standard window formating
-#     faceReg = Toplevel()
-#     faceReg.title('Face registration')
-#     faceReg.geometry("500x350")
-#     welcome = Label(faceReg, text="Hi").pack()
-#     # face registration window specific
-#     user = Entry(faceReg, width=50).pack()
-
- 
-
-
-
-# def faceRec_window():
-#     # Standard window formating
-#     faceRec = Toplevel()
-#     faceRec.title("Face recognition")
-#     faceRec.geometry("500x350")
-#     welcome = Label(faceRec, text="Hi this is face rec").pack()
-#     # 
-
-
-
-faceReg_button = Button(root, text="Register your face here", command=openFaceReg).pack()
-# faceRec_button = Button(root, text="Main face registration program", command=faceRec_window).pack()
-# button_quit = Button(root, text="Exit program", command=root.quit).pack()
+root = MyApp()
+root.withdraw()
+root.title("Main page, rename at bottom of script.")
 root.mainloop()
--- guiTest.py
+++ guiTest.py
@@ -1,86 +1,114 @@
-import shutil
-import cv2
-import os
-from tkinter import *
+from http.client import OK
 from time import sleep
+from tkinter import *
+from tkinter import messagebox
+from tkinter import ttk
+import os
+import cv2
+# from guiTest import name_collection
 
-# functions
+root = Tk()
+root.title('Facial recognition GUI')
+root.geometry("500x350")
 
-    # name collection and validation.
-def name_collection(user):
-        # Validation: At least one alphabet character, allows for spaces.
-    if(any(x.isalpha() for x in user)
-    and all(x.isalpha() or x.isspace() for x in user)):
-        # Converts user to lower case and whitespaces to '-'
-        user = user.lower()
-        user = user.replace(" ","-")
-        username = user
-        # Verificiation that username is what user intended.
-        print("The name you entered is: {}\n".format(user))
-        print("Would you like to continue with the name '{}'?\n".format(username))
-        a = input('Type yes or y and press enter to continue:')
-        # Begins to check to see if the user already exists in the dataset.
-        parent_directory = "dataset/"
-        path = os.path.join(parent_directory, username)
-        if a == "yes" or a == "y":
+def openFaceReg():
+    faceReg = Toplevel()
+    faceReg.title('Face registration')
+    faceReg.geometry("500x350")
+    welcome = Label(faceReg, text ="Welcome, please enter name.").pack()
+
+    def name_collection():
+        if(any(x.isalpha() for x in user_name.get())
+        and all(x.isalpha() or x.isspace() for x in user_name.get())):
+            username = user_name.get()
+            username = username.lower()
+            username = username.replace(" ","-")
+            parent_directory = "dataset/"
+            path = os.path.join(parent_directory, username)
             if os.path.isdir(path):
-                print("This user already exists, please try again.\n")
-                return 1
+                messagebox.showerror("Existing user", "This user is already a part of the system.")
             else:
-                return username # Returns username if it meets all criteria.
+                os.mkdir(path)
+                Label(faceReg, text=f'The directory {username} has been created.').pack()
+                sleep(1)
+                image_collection(path)
+                #possiblly close this window and open one for image collection?
         else:
-            return 1
-    else:
-        print("Please try again.")
-        return 1
+            messagebox.showerror("Invalid characters detected.", "Invalid characters in username\n letters and white space only.")
+
+    def image_collection(path):
+        face_cascade = cv2.CascadeClassifier('cascades/haarcascade_frontalface_default.xml')
+        cap = cv2.VideoCapture(0, cv2.CAP_V4L2)
+        collection_progess = ttk.Progressbar(faceReg, orient=HORIZONTAL, length=300, mode='determinate')
+        collection_progess.pack()
+        for i in range(1, 4):
+            while(True):
+                ret, frame = cap.read()
+                frame = cv2.flip(frame, 0)
+                sleep(1)
+                break
+            sleep(2)
+            while(True):
+                # Capture frame-by-frame
+                ret, frame = cap.read()
+                frame = cv2.flip(frame, 0)
+                gray  = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
+                faces = face_cascade.detectMultiScale(gray, scaleFactor=1.5, minNeighbors=5)
+                if len(faces) == 1:
+                    message = f'Face detected, sample {i}/30 successful.'
+                    break
+                elif len(faces) < 1:
+                    message = "No face detected, please adjust."
+                else:
+                    message = "Too many faces detected, there should only be one person in frame."
+            cv2.imwrite("{}/test{}.png".format(path, i), frame)
+            sleep(1.6)
+            message_label = Label(faceReg, text = message)
+            message_label.pack()
+            collection_progess['value'] += 33
+            faceReg.update_idletasks()
+        sleep(2)
+        finsih = messagebox.showinfo('User setup complete.','Press OK to finish.')
+        if finsih == "ok":
+            faceReg.destroy()
+    # End of face registration functions.
+
+    # style this window and change variable names.
+    user_name = Entry(faceReg, width=30)
+    user_name.pack(pady=30)
+    Button(faceReg, text="register player", padx=10, pady=5, command= name_collection).pack()
+
+# End of face registration window.
+    
+    
 
 
-#     # Directory creation function for dataset.
-# def directory_creation(username):
-#     parent_directory = "dataset/"
-#     path = os.path.join(parent_directory, username)
-#     os.mkdir(path)
-#     print("Directory {} has been created.\n".format(path))
-
-#     # Function that removes username from dataset.
-# def directory_remove(path):
-#     shutil.rmtree(path)
-#     print("Directory {} has been removed.\n".format(newUser))
-
-#     # Saves a frame that has a face detected.
-# def save_frame(frame, i):
-#     cv2.imwrite('dataset/{}/test{}.png'.format(newUser, i), frame)
-
-#     # Detects image and calls save frame function.
-# def image_collection():
-#     for i in range(1, 31):
-#         while(True):
-#             ret, frame = cap.read()
-#             frame = cv2.flip(frame, 0)
-#             sleep(1)
-#             break
-#         sleep(2.4)
-#         while(True):
-#             # Capture frame-by-frame
-#             ret, frame = cap.read()
-#             frame = cv2.flip(frame, 0)
-#             gray  = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
-#             faces = face_cascade.detectMultiScale(gray, scaleFactor=1.5, minNeighbors=5)
-#             x = 0
-#             if len(faces) == 1:
-#                 break
-#             elif len(faces) < 1:
-#                 print("No faces detected, please adjust.")
-#             else:
-#                 print("Too many faces detected, there should only be one person in frame.")
-#         save_frame(frame, i)
-#         print("Face detected, sample {}/30 successful.".format(i))
-#         sleep(1.6)
-#     if x == 0:
-#         print("sample collection successful...\nUser {} added to dataset.".format(newUser))
-#     else:
-#         print("\n")
 
 
-# face_cascade = cv2.CascadeClassifier('cascades/haarcascade_frontalface_default.xml')
-# cap = cv2.VideoCapture(0, cv2.CAP_V4L2)
+# def faceReg_window():
+#     # Standard window formating
+#     faceReg = Toplevel()
+#     faceReg.title('Face registration')
+#     faceReg.geometry("500x350")
+#     welcome = Label(faceReg, text="Hi").pack()
+#     # face registration window specific
+#     user = Entry(faceReg, width=50).pack()
+
+ 
+
+
+
+# def faceRec_window():
+#     # Standard window formating
+#     faceRec = Toplevel()
+#     faceRec.title("Face recognition")
+#     faceRec.geometry("500x350")
+#     welcome = Label(faceRec, text="Hi this is face rec").pack()
+#     # 
+
+
+
+faceReg_button = Button(root, text="Register your face here", command=openFaceReg).pack()
+# faceRec_button = Button(root, text="Main face registration program", command=faceRec_window).pack()
+# button_quit = Button(root, text="Exit program", command=root.quit).pack()
+root.mainloop()
