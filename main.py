34bfba71b5aa7fab80ca404132665d9da6ceaaed
Cameron Griffiths <camgriff01@gmail.com>
2022-04-28 19:04:14 +0100

    main now works on a basic face verification

--- main.py
+++ main.py
@@ -15,39 +15,35 @@ with open("identities/face-labels.pickle", 'rb') as f:
 cap = cv2.VideoCapture(0, cv2.CAP_V4L2)
 time.sleep(0.1)
 
-while(True):
-    # Capture frame-by-frame
-    ret, frame = cap.read()
-    frame = cv2.flip(frame, 0)
-    gray  = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
-    faces = face_cascade.detectMultiScale(gray, scaleFactor=1.5, minNeighbors=5)
-    for (x, y, w, h) in faces:
-        # print(x,y,w,h)
-        roi_gray = gray[y:y+h, x:x+w] #(ycord_start, ycord_end)
-        roi_color = frame[y:y+h, x:x+w]
-
-        #After prediction of ROI (Region of interest) computation is focused in that area.
-        id_, conf = recogniser.predict(roi_gray)
-        if conf>=40 and conf <= 80:
-            font = cv2.FONT_HERSHEY_SIMPLEX
-            name = labels[id_]
-            color = (255, 255, 255)
-            stroke = 2
-            confidence = str(conf)
-            cv2.putText(frame, name, (x,y), font, 1, color, stroke, cv2.LINE_AA)
-            cv2.putText(frame, confidence, (y,x), font, 1, color, stroke, cv2.LINE_AA)
-        color = (255, 0, 0) #BGR 0-255 
-        stroke = 2
-        end_cord_x = x + w
-        end_cord_y = y + h
-        cv2.rectangle(frame, (x, y), (end_cord_x, end_cord_y), color, stroke)
-    	#subitems = smile_cascade.detectMultiScale(roi_gray)
-    	#for (ex,ey,ew,eh) in subitems:
-    	#	cv2.rectangle(roi_color,(ex,ey),(ex+ew,ey+eh),(0,255,0),2)
-    # Display the resulting frame
-    cv2.imshow('Face detection',frame)
-    if cv2.waitKey(20) & 0xFF == ord('q'):
+USER = "user zero two"
+detected = 1
+while detected <= 10:
+    while(True):
+        # Capture frame-by-frame
+        ret, frame = cap.read()
+        frame = cv2.flip(frame, 0)
+        gray  = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
+        faces = face_cascade.detectMultiScale(gray, scaleFactor=1.5, minNeighbors=5)
+        for (x, y, w, h) in faces:
+            # print(x,y,w,h)
+            roi_gray = gray[y:y+h, x:x+w] #(ycord_start, ycord_end)
+            roi_color = frame[y:y+h, x:x+w]
+            #After prediction of ROI (Region of interest) computation is focused in that area.
+            id_, conf = recogniser.predict(roi_gray)
+            if conf>=40 and conf <= 80:
+                name = labels[id_]
+                name = name.replace("-", " ")
+                USER = USER.lower()
+                if name == USER:
+                    print(F'{USER} detected.')
+                    detected +=1
+           
+        cv2.imshow('Face detection',frame)
+        if cv2.waitKey(20) & 0xFF == ord('q'):
+            cap.release()
+            cv2.destroyAllWindows()
         break
+    
 
 # When everything done, release the capture
 cap.release()
