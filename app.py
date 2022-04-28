41a5ced7ffb963dd6262ac59d0e21ee66afa741b
Cameron Griffiths <camgriff01@gmail.com>
2022-04-28 19:03:35 +0100

    GUI now working for verification based on face!

--- app.py
+++ app.py
@@ -1,8 +1,12 @@
 import tkinter as tk
-from tkinter import messagebox
+from tkinter import Label, messagebox
 from tkinter import ttk
-
 from numpy import roots
+import os
+import cv2
+from time import sleep
+from PIL import Image
+import pickle
 
 
 class LoginPage(tk.Tk): # modify for face recognition instead of pswd, then add if fails to use pswd
@@ -53,62 +57,122 @@ class LoginPage(tk.Tk): # modify for face recognition instead of pswd, then add 
             else:
                 tk.messagebox.showerror("Information", "The username or password you have entered are incorrect ")
         def validate(username, password):
-            try:
-                with open("credentials.txt", "r") as credentials:
-                    for line in credentials:
-                        line = line.split(",")
-                        if line [1] == username and line[3] == password:
-                            return True
-                    return False
-            except FileNotFoundError:
-                print("you need to register first")
-                return False
+            face_cascade = cv2.CascadeClassifier('cascades/haarcascade_frontalface_default.xml')
+            recogniser = cv2.face.LBPHFaceRecognizer_create()
+            recogniser.read("recognisers/face-trainner.yml")
+
+            labels = {"person_name": 1}
+            with open("identities/face-labels.pickle", 'rb') as f:
+                og_labels = pickle.load(f)
+                labels = {v:k for k,v in og_labels.items()}
+
+            cap = cv2.VideoCapture(0, cv2.CAP_V4L2)
+            sleep(0.1)
+            detected = 1
+            while detected <= 10:
+                while(True):
+                    # Capture frame-by-frame
+                    ret, frame = cap.read()
+                    frame = cv2.flip(frame, 0)
+                    gray  = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
+                    faces = face_cascade.detectMultiScale(gray, scaleFactor=1.5, minNeighbors=5)
+                    for (x, y, w, h) in faces:
+                        # print(x,y,w,h)
+                        roi_gray = gray[y:y+h, x:x+w] #(ycord_start, ycord_end)
+                        roi_color = frame[y:y+h, x:x+w]
+                        #After prediction of ROI (Region of interest) computation is focused in that area.
+                        id_, conf = recogniser.predict(roi_gray)
+                        if conf>=40 and conf <= 80:
+                            name = labels[id_]
+                            name = name.replace("-", " ")
+                            USER = username.lower()
+                            if name == USER:
+                                print(F'{USER} detected.')
+                                detected +=1
+                    
+                    cv2.imshow('Face detection',frame)
+                    if cv2.waitKey(20) & 0xFF == ord('q'):
+                        cap.release()
+                        cv2.destroyAllWindows()
+                    break
+            # When everything done, release the capture
+            cap.release()
+            cv2.destroyAllWindows()
+            return True
+
+        # def validate_password(username, password):
+        #     try:
+        #         with open("credentials.txt", "r") as credentials:
+        #             for line in credentials:
+        #                 line = line.split(",")
+        #                 if line [1] == username and line[3] == password:
+        #                     return True
+        #             return False
+        #     except FileNotFoundError:
+        #         print("you need to register first")
+        #         return False
 
 class SignupPage(tk.Tk):        # incorporate the face registration process here
     def __init__(self, *args, **kwargs):
         tk.Tk.__init__(self, *args, **kwargs)
 
-        main_frame = tk.Frame(self, bg="#3F6BAA", height=150, width=250)
-        # pack_propagate prevents the window resizing to match the widgets
+        main_frame = tk.Frame(self, bg="#FFF", height = 431, width=626)
         main_frame.pack_propagate(0)
-        main_frame.pack(fill="both", expand="true")
+        main_frame.pack(fill="both", expand = "true")
+        # pack_propagate prevents the window resizing to match the widgets
+        
 
-        self.geometry("250x150")
+        self.geometry("450x350")
         self.resizable(0, 0)
 
         self.title("Registration")
+        addUser_frame = tk.Frame(main_frame, bg="#FFF", relief="groove", bd=2)
+        addUser_frame.place(rely=0.30, relx=0.17, height=130, width=400)
 
-        label_user = tk.Label(main_frame, text="New Username:")
+        label_user = tk.Label(addUser_frame, text="New Username:")
         label_user.grid(row=1, column=0)
 
-        label_pw = tk.Label(main_frame, text="New Password:")
+        label_pw = tk.Label(addUser_frame, text="New Password:")
         label_pw.grid(row=2, column=0)
 
-        entry_user = ttk.Entry(main_frame, width=20, cursor="xterm")
+        entry_user = ttk.Entry(addUser_frame, width=20, cursor="xterm")
         entry_user.grid(row=1, column=1)
 
-        entry_pw = ttk.Entry(main_frame, width=20, cursor="xterm", show="*")
+        entry_pw = ttk.Entry(addUser_frame, width=20, cursor="xterm", show="*")
         entry_pw.grid(row=2, column=1)
 
-        button = ttk.Button(main_frame, text="Create Account", command=lambda: signup())
+        button = ttk.Button(addUser_frame, text="Create Account", command=lambda: signup())
         button.grid(row=4, column=1)
+
+        display_label = Label(addUser_frame)
+        display_label.grid(row=5, column=1)
     
         def signup():
             user = entry_user.get()
             pw = entry_pw.get()
             validation = validate_user(user)
             if not validation:
-                tk.messagebox.showerror("information", "Username already exists")
+                tk.messagebox.showerror("information", "Invlid character or username already exists")
             else:
                 if len(pw) > 3:
                     credentials = open("credentials.txt", "a")
                     credentials.write(f"Username,{user},Password,{pw},\n")
                     credentials.close()
-                    tk.messagebox.showinfo("Information", "Your account details have been stored.")
-                    SignupPage.destroy(self)
+                    parent_directory = "dataset/"
+                    user = user.lower()
+                    user = user.replace(" ","-")
+                    path = os.path.join(parent_directory, user)
+                    if os.path.isdir(path):
+                        messagebox.showerror("Existing user", "This user is already a part of the system.")
+                    else:
+                        os.mkdir(path)
+                        tk.messagebox.showinfo("Information", "Your account details have been stored.\nFace registration will now begin.")
+                        image_collection(path)
                 else:
                     tk.messagebox.showerror("information", "Your password is too short")
         def validate_user(username):
+            if(any(x.isalpha() for x in username)
+            and all(x.isalpha() or x.isspace() for x in username)):
                 # Checks the text file for a username/password combination.
                 try:
                     with open("credentials.txt", "r") as credentials:
@@ -119,6 +183,50 @@ class SignupPage(tk.Tk):        # incorporate the face registration process here
                     return True
                 except FileNotFoundError:
                     return True
+            else:
+                return False
+        def image_collection(path):
+            face_cascade = cv2.CascadeClassifier('cascades/haarcascade_frontalface_default.xml')
+            cap = cv2.VideoCapture(0, cv2.CAP_V4L2)
+            # collection_progess = ttk.Progressbar(self, orient=HORIZONTAL, length=300, mode='determinate')
+            # collection_progess.pack()
+            for i in range(1, 4):
+                while(True):
+                    ret, frame = cap.read()
+                    frame = cv2.flip(frame, 0)
+                    sleep(1)
+                    break
+                sleep(2)
+                while(True):
+                    # Capture frame-by-frame
+                    ret, frame = cap.read()
+                    show_display(frame)
+                    frame = cv2.flip(frame, 0)
+                    gray  = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
+                    faces = face_cascade.detectMultiScale(gray, scaleFactor=1.5, minNeighbors=5)
+                    if len(faces) ==1:
+                        break
+                    elif len(faces) < 1:
+                        print("No faces detected.")
+                    else:
+                        print("Too many faces detected.")
+                cv2.imwrite("{}/test{}.png".format(path, i), frame)
+                sleep(1.6)
+                # message_label = Label(faceReg, text = message)
+                # message_label.pack()
+                # collection_progess['value'] += 33
+                # faceReg.update_idletasks()
+            sleep(2)
+            finsih = messagebox.showinfo('User setup complete.','Press OK to finish.')
+            cap.release()
+            cv2.destroyAllWindows()
+            if finsih == "ok":
+                SignupPage.destroy(self)    
+        def show_display(source):
+            cv2.imshow('Face registration', source)
+            if cv2.waitKey(20) & 0xFF == ord('q'):
+                cv2.destroyAllWindows()
+                SignupPage.destroy(self) 
 class MyApp(tk.Tk): # no idea what to put here
 
     def __init__(self, *args, **kwargs):
